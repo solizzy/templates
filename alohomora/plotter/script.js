3a7e0db670c94ca622f71505c9d7e5da4664c179
Isabella Broch <isabrochweb@gmail.com>
2021-10-12 14:23:42 +0200

    Handle hash linking

--- alohomora/plotter/script.js
+++ alohomora/plotter/script.js
@@ -1,7 +1,19 @@
+const hash = location.hash.replace("#", "");
+const targets = $(".index button").get().map( el => el.dataset.for );
+
+if (targets.includes(hash)) {
+	toggleItem(hash);
+}
+
 $(".izzy.plotter").on("click", ".index button", function() {
+	const target = $(this).attr("data-for");
+	toggleItem(target);
+})
+
+function toggleItem(target) {
 	$(".izzy.plotter .index button").removeClass("active");
 	$(".izzy.plotter .content article").removeClass("active");
-	const target = $(this).attr("data-for");
-	$(this).addClass("active");
-	$(`.izzy.plotter .content article[data-label="${target}"]`).addClass("active");
-})
+	$(`.izzy.plotter .index [data-for="${target}"]`).addClass("active");
+	$(`.izzy.plotter .content [data-label="${target}"]`).addClass("active");
+	location.hash = target;
+}
